60d1df3f4b5941f5b16e4d92964c1d3779395843
Trivial-lore <triviallore@gmail.com>
2024-07-20 20:08:22 -0500

    fabric js setup basic

--- features/editor/components/editor.tsx
+++ features/editor/components/editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { fabric } from "fabric";
 import { useEffect, useRef } from "react";
 import { useEditor } from "../hooks/use-editor";
 
@@ -12,15 +13,22 @@ const Editor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      controlsAboveOverlay: true,
+      preserveObjectStacking: true,
+    });
+
     init({
-      initialCanvas: "",
+      initialCanvas: canvas,
       initialContainer: containerRef.current!,
     });
   }, [init]);
 
   return (
-    <div ref={containerRef}>
-      <canvas ref={canvasRef}/>
+    <div className="h-full flex flex-col">
+      <div className="flex-1 h-full bg-muted" ref={containerRef}>
+        <canvas ref={canvasRef} />
+      </div>
     </div>
   );
 };
--- features/editor/hooks/use-editor.ts
+++ features/editor/hooks/use-editor.ts
@@ -1,16 +1,48 @@
 import { useCallback } from "react";
+import { fabric } from "fabric";
 
 export const useEditor = () => {
+  const init = useCallback(
+    ({
+      initialCanvas,
+      initialContainer,
+    }: {
+      initialCanvas: fabric.Canvas;
+      initialContainer: HTMLDivElement;
+    }) => {
+      // Set the fabric object control defaults
+      fabric.Object.prototype.set({
+        cornerColor:"#FFF",
+        cornerStyle: "circle",
+        borderColor: "#3b82f6",
+        borderScaleFactor: 1.5,
+        transparentCorners: false,
+        borderOpacityWhenMoving: 1,
+        cornerStrokeColor: "#3b82f6",
+      })
+      // Set the initial canvas dimensions
+      const initialWorkspace = new fabric.Rect({
+        width:900,
+        height:1200,
+        name: "clip",
+        fill: "white",
+        selectable: false,
+        hasControls: false,
+        shadow: new fabric.Shadow({
+          color: "rgba(0, 0, 0, 0.8)",
+          blur: 5,
+        }),
+      });
 
-  const init = useCallback(({
-    initialCanvas,
-    initialContainer,
-  }: {
-    initialCanvas: any,
-    initialContainer: HTMLDivElement,
-  }) => {
-    console.log("Initializing editor");
-  }, []);
+      initialCanvas.setWidth(initialContainer.offsetWidth);
+      initialCanvas.setHeight(initialContainer.offsetHeight);
+
+      initialCanvas.add(initialWorkspace);
+      initialCanvas.centerObject(initialWorkspace);
+      initialCanvas.clipPath = initialWorkspace;
+    },
+    []
+  );
 
   return { init };
 };
