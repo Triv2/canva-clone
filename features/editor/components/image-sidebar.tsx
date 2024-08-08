8aa646e4e163ea570dc9dd45d964c6d330dafe1e
Trivial-lore <triviallore@gmail.com>
2024-08-08 10:51:34 -0500

    uploadthing setup

--- features/editor/components/image-sidebar.tsx
+++ features/editor/components/image-sidebar.tsx
@@ -5,6 +5,7 @@ import { ToolSidebarClose } from "./tool-sidebar-close";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useGetImages } from "@/features/images/api/use-get-images";
 import { AlertTriangle, Loader } from "lucide-react";
+import { UploadButton } from "@/lib/uploadthing";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -33,9 +34,24 @@ export const ImageSidebar = ({
       )}
     >
       <ToolSidebarHeader
-        title="Image"
+        title="Images"
         description="Add images to your canvas"
       />
+      <div className="p-4 border-b">
+        <UploadButton
+          appearance={{
+            button: "w-full text-sm font-medium",
+            allowedContent: "hidden",
+          }}
+          content={{
+            button:"Upload Image",
+          }}
+          endpoint="imageUploader"
+          onClientUploadComplete={(res)=> {
+            editor?.addImage(res[0].url);
+          }}
+        />
+      </div>
       {isLoading && (
         <div className="flex items-center justify-center flex-1">
           <Loader className="size-4 text-muted-foreground animate-spin" />
@@ -52,30 +68,29 @@ export const ImageSidebar = ({
       <ScrollArea>
         <div className="p-4">
           <div className="grid grid-cols-2 gap-4">
-          {data &&
-            data.map((image) => {
-              return (
-                <button
-                  key={image.id}
-                  className="relative w-full h-[100px] group hover:opacity-75 transition bg-muted rounded-sm overflow-hidden border"
-                  onClick={()=>editor?.addImage(image.urls.regular)}
-                >
-                 <Image
-                  fill
-                  src={image.urls.small}
-                  alt={image.alt_description || "Image"}
-                  className="object-cover"
-                 />
-                 <Link
-                  target="_blank"
-                  href={image.links.html}
-                  className="opacity-0 group-hover:opacity-100 absolute left-0 bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50 text-left"
-                 >
-                 </Link>
-                </button>
-              );
-            })}
-            </div>
+            {data &&
+              data.map((image) => {
+                return (
+                  <button
+                    key={image.id}
+                    className="relative w-full h-[100px] group hover:opacity-75 transition bg-muted rounded-sm overflow-hidden border"
+                    onClick={() => editor?.addImage(image.urls.regular)}
+                  >
+                    <Image
+                      fill
+                      src={image.urls.small}
+                      alt={image.alt_description || "Image"}
+                      className="object-cover"
+                    />
+                    <Link
+                      target="_blank"
+                      href={image.links.html}
+                      className="opacity-0 group-hover:opacity-100 absolute left-0 bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50 text-left"
+                    ></Link>
+                  </button>
+                );
+              })}
+          </div>
         </div>
       </ScrollArea>
       <ToolSidebarClose onClick={onClose} />
