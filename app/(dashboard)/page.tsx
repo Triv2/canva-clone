94aeec5bea2b8d359ef1c9778b8006820ab53da0
Trivial-lore <triviallore@gmail.com>
2024-08-21 09:55:27 -0500

    home page layout setup

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,16 @@
-
 import { protectServer } from "@/features/auth/utils";
+
+import { Banner } from "./banner";
 
 
 export default async function Home() {
   await protectServer();
 
   return (
-    <div>
-      Logged In
+    <div className="flex flex-col space-y-6 max-w-screen-xl mx-auto pb-10">
+      <Banner />
+ 
     </div>
   );
-}
+};
+
